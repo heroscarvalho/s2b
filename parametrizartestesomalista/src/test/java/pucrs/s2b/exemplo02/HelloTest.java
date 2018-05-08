901c31e47c02972d338a2670840ec18372554f13
heroscarvalho <heros_gabriel@Hotmail.com>
2018-05-08 15:09:18 -0300

    #2*

---
+++ parametrizartestesomalista/src/test/java/pucrs/s2b/exemplo02/HelloTest.java
@@ -0,0 +1,28 @@
+package pucrs.s2b.exemplo02;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
+
+@RunWith(JUnitParamsRunner.class)
+
+public class HelloTest {
+	
+	@Test 
+	@Parameters({"20, 30, 50", 
+		  "0, 0, 0" })
+	  
+	public void testSomarP(int a, int b, int r) {
+		
+		int expected =r;
+		
+		int actual = Hello.somar(a, b);
+		
+		assertEquals(expected, actual);
+	}
+	
+}
